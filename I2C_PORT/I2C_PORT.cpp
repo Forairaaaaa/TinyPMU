3b56df6b4f38b5723b38c8f7d0dc5467a520f8ea
Forairaaaaa <applesyqd@outlook.com>
2022-06-25 11:20:20 +0800

    fix warning

--- I2C_PORT/I2C_PORT.cpp
+++ I2C_PORT/I2C_PORT.cpp
@@ -13,38 +13,33 @@ bool I2C_PORT::_I2C_checkDevAvl() {
     return ( _wire->endTransmission() ? true : false);
 }
 
+void I2C_PORT::_I2C_write1Byte(uint8_t addr, uint8_t data) {
+    _wire->beginTransmission(_dev_addr);
+    _wire->write(addr);
+    _wire->write(data);
+    _wire->endTransmission();
+}
+
+uint8_t I2C_PORT::_I2C_read8Bit(uint8_t addr) {
+    _wire->beginTransmission(_dev_addr);
+    _wire->write(addr);
+    _wire->endTransmission();
+    _wire->requestFrom(_dev_addr, (size_t)1);
+    return _wire->read();
+}
+
+void I2C_PORT::_I2C_readBuff(uint8_t addr, int size, uint8_t buff[]) {
+    _wire->beginTransmission(_dev_addr);
+    _wire->write(addr);
+    _wire->endTransmission();
+    _wire->requestFrom(_dev_addr, (size_t)size);
+    for (int i = 0; i < size; i++) {
+        buff[i] = _wire->read();
+    }
+}
+
 #else
 void I2C_PORT::_I2C_init(uint8_t dev_addr) { }
 void I2C_PORT::_I2C_checkDevAvl() { }
 #endif
 
-void I2C_PORT::_I2C_write1Byte(uint8_t addr, uint8_t data) {
-    #ifdef ARDUINO
-    _wire->beginTransmission(_dev_addr);
-    _wire->write(addr);
-    _wire->write(data);
-    _wire->endTransmission();
-    #endif
-}
-
-uint8_t I2C_PORT::_I2C_read8Bit(uint8_t addr) {
-    #ifdef ARDUINO
-    _wire->beginTransmission(_dev_addr);
-    _wire->write(addr);
-    _wire->endTransmission();
-    _wire->requestFrom(_dev_addr, 1);
-    return _wire->read();
-    #endif
-}
-
-void I2C_PORT::_I2C_readBuff(uint8_t addr, int size, uint8_t buff[]) {
-    #ifdef ARDUINO
-    _wire->beginTransmission(_dev_addr);
-    _wire->write(addr);
-    _wire->endTransmission();
-    _wire->requestFrom(_dev_addr, size);
-    for (int i = 0; i < size; i++) {
-        buff[i] = _wire->read();
-    }
-    #endif
-}
